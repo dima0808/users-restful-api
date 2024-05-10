2817ed061a5e22bcaa813077890dc3a8db0f9602
dimitri <mamchenko2210@gmail.com>
2024-05-10 18:41:59 +0200

    init commit

---
+++ src/main/java/com/company/usersrestfulapi/UsersRestfulApiApplication.java
@@ -0,0 +1,13 @@
+package com.company.usersrestfulapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersRestfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersRestfulApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/usersrestfulapi/UsersRestfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.usersrestfulapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersRestfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
